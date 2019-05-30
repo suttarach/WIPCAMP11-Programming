24fcbd3e775d5559b0ef6ad44e2fe14fbd29109d
suttarach <suttarach@gmail.com>
2019-05-30 11:38:14 +0700

    [ADD] New Project

---
+++ WIP-XI/src/III/Output/Print.java
@@ -0,0 +1,23 @@
+/*
+ * WIP XI Computer Programing 
+ */
+
+package III.Output;
+
+/**
+ *
+ * @author sittiwatlcp
+ */
+public class Print {
+    public static void main(String[] args) {
+        System.out.print("เราชื่อโฉม \n");
+        
+        System.out.print("มีเพื่อนข้างๆ \n");
+        
+        // \n <-- new line (ชี้นบรรทัดใหม่)
+        System.out.print("ชื่อว่า มิ้นต์ \n");
+        
+        System.out.print("พวกเราอยู่มันม่วง เย่ๆ");
+        
+    }
+}
